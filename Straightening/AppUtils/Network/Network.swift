8953cf4603209849639b90e6faca45930b9a557a
Josicleison <engenhariaeletricaquinto@gmail.com>
2022-11-09 14:32:33 -0300

    First commit my architecture

---
+++ Straightening/AppUtils/Network/Network.swift
@@ -0,0 +1,34 @@
+//
+//  Network.swift
+//  News
+//
+//  Created by Josicleison on 10/10/22.
+//
+
+import Foundation
+
+struct Network {
+        
+    static func call(from url: String?) async -> Data? {
+
+        guard let url = URL(string: url ?? "") else {print("ERROR: Wrong url"); return nil}
+
+        do {
+
+            let (data, response) = try await URLSession.shared.data(from: url)
+            print("RESPONSE:", response)
+
+            return data
+        }
+        catch {print("ERROR: \(error)"); return nil}
+    }
+
+    static func decode<T: Codable>(_ what: T.Type, from data: Data) -> T? {
+
+        do {
+
+            return try JSONDecoder().decode(what.self, from: data)
+        }
+        catch {print("ERROR: \(error)"); return nil}
+    }
+}
