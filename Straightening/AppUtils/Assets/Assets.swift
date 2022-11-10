79aafb480f93fca1b309487fca19fe803b2555d9
Josicleison <engenhariaeletricaquinto@gmail.com>
2022-11-10 14:01:49 -0300

    Added Create object to create default objects, and Assets to give direct asses to assets by singleton

---
+++ Straightening/AppUtils/Assets/Assets.swift
@@ -0,0 +1,13 @@
+//
+//  Assets.swift
+//  Straightening
+//
+//  Created by Josicleison Elves on 19/08/1401 AP.
+//
+
+import Foundation
+
+struct Assets {
+    
+    
+}
---
+++ Straightening/AppUtils/Create/Create.swift
@@ -0,0 +1,13 @@
+//
+//  Create.swift
+//  Straightening
+//
+//  Created by Josicleison Elves on 19/08/1401 AP.
+//
+
+import Foundation
+
+struct Create {
+    
+    
+}
