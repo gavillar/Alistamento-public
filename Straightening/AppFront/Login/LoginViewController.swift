e21c3d3e91c1cf4ee8ed297948d37d667e70c325
VillarDeveloper <villar.developer@gmail.com>
2022-11-11 05:10:25 -0800

    setup

--- Straightening/AppFront/Login/LoginViewController.swift
+++ Straightening/AppFront/Login/LoginViewController.swift
@@ -8,21 +8,64 @@
 import Foundation
 import UIKit
 
-class LoginViewController: UIViewController {
-    private let logo: (stack: UIStackView, label: UILabel) = {
+class LoginViewController: UIViewController, SetupView {
+    private lazy var logo: (stack: UIStackView, label: UILabel) = {
+        
         let label = UILabel()
-        return (stack: UIStackView(arrangedSubviews: [label]), label: label)
+        label.text = "Alistamento"
+        label.textColor = .black
+        
+        let stack = UIStackView(arrangedSubviews: [label])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return (stack: stack, label: label)
+        
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        setupConstrains()
+    }
+    
+    private func gradientView() {
+        
         let gradient = CAGradientLayer()
 
         gradient.frame = view.bounds
         gradient.colors = [UIColor.systemGreen.cgColor, UIColor.systemGreen.cgColor, UIColor.white.cgColor]
 
         view.layer.insertSublayer(gradient, at: 0)
-        view.addSubview(logo.stack)
-        logo.stack.constraint(attributesConstants: [.top: 100, .leading: 30])
+        
+    }
+    
+     func setupView() {
+        gradientView()
+        view.addSubviews([logo.stack])
+
+    }
+
+    
+    
+    
+    
+     func setupConstrains() {
+        
+        
+        NSLayoutConstraint.activate([
+        
+          
+            logo.stack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            logo.stack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        
+        
+        
+        
+        
+        
+        ])
+        
+        
     }
 }
+
+
---
+++ Straightening/Setup/SetupView.swift
@@ -0,0 +1,17 @@
+//
+//  SetupView.swift
+//  Straightening
+//
+//  Created by user220831 on 11/10/22.
+//
+
+import Foundation
+
+
+protocol SetupView {
+    
+    func setupView()
+    func setupConstrains()
+    
+    
+}
