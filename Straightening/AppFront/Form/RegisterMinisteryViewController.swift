b8157942699f266f47cfb23f172dce5961e44506
Josicleison <engenhariaeletricaquinto@gmail.com>
2022-11-14 15:51:05 -0300

    Added pickerView

--- Straightening/AppFront/Form/RegisterMinisteryViewController.swift
+++ Straightening/AppFront/Form/RegisterMinisteryViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class RegisterMinisteryViewController: RegisterNameViewController {
+    var options = ["Pastoral", "Missões", "Família", "Discipulado", "Serviço", "Adoração" ]
+    lazy var pickerView: UIPickerView = {
+        let pickerView = UIPickerView()
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        return pickerView
+    }()
     override func loadView() {
         super.loadView()
         textField.attributedPlaceholder = NSAttributedString(string: "⇩ Ministério",
@@ -21,5 +28,34 @@ class RegisterMinisteryViewController: RegisterNameViewController {
                                                           animated: true)
         }
         button.addAction(action, for: .touchUpInside)
+        
+        textField.inputView = pickerView
+        
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.sizeToFit()
+
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneButtonTapped))
+        toolBar.setItems([spaceButton, doneButton], animated: false)
+        textField.inputAccessoryView = toolBar
+    }
+    @objc func doneButtonTapped() {
+        textField.resignFirstResponder()
+    }
+}
+
+extension RegisterMinisteryViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return options.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return options[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        textField.text = options[row]
     }
 }
