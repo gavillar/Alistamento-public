217ea88aefddfedf9d3a4b5e0a7781ff26228dbd
Josicleison <engenhariaeletricaquinto@gmail.com>
2022-11-17 14:02:21 -0300

    used network call function to call books

--- Straightening/AppFront/Bible/Books/BooksViewModel.swift
+++ Straightening/AppFront/Bible/Books/BooksViewModel.swift
@@ -24,25 +24,12 @@ class BooksViewModel {
         return self.booksData.count
     }
     
-
     func getBooks(completion: @escaping (Books) -> Void) {
-        
-        guard let url = URL(string: "https://www.abibliadigital.com.br/api/books") else {return}
-       
-        URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in 
-            if error == nil {
-                print("not nil")
-            }
-                guard let data = data else {return}
-                
-                let result = try? JSONDecoder().decode(Books.self, from: data)
-            print("status 200")
-                
-                guard let newResult = result else {return}
-            
-            self?.booksviewmodeldelegate?.sendBooksQuantity(data: newResult)
-                
-        }.resume()
+        Task {[weak self] in
+            guard let data = await Network.call(from: "https://www.abibliadigital.com.br/api/books") else {return}
+            guard let books = Network.decode(Books.self, from: data) else {return}
+            self?.booksviewmodeldelegate?.sendBooksQuantity(data: books)
+        }
     }
 }
     
