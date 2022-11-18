724ef6f9876d2e12e429ebabf4673b5ca7a6ede2
Josicleison <engenhariaeletricaquinto@gmail.com>
2022-11-18 08:59:58 -0300

    refactored to default with swiftLint

--- Straightening/AppFront/IntroView/IntroViewController.swift
+++ Straightening/AppFront/IntroView/IntroViewController.swift
@@ -5,27 +5,23 @@
 //  Created by user220831 on 11/16/22.
 //
 
-import Foundation
 import UIKit
 
 class IntroViewController: UIViewController, SetupView {
-    
     let loginviewcontroller = LoginViewController()
     let booksviewcontroller = BooksViewController()
-    
-    private lazy var straighteningButton = Create.baseButton("Alistamento", titleColor: .black, backgroundColor: Assets.Colors.green, for: .touchUpInside) {_ in
+    private lazy var straighteningButton = Create.baseButton("Alistamento", titleColor: .black,
+                                                             backgroundColor: Assets.Colors.green) {_ in
 //            self.navigationController?.pushViewController(self.loginviewcontroller, animated: true)
 //        self.navigationController?.pushViewController(self.loginviewcontroller, animated: true)
         self.navigationController?.navigate(to: self.loginviewcontroller)
     }
-    
-    private lazy var bibleButton = Create.baseButton("Biblía Sagrada", titleColor: .black, backgroundColor: Assets.Colors.green, for: .touchUpInside, handler: {_ in
+    private lazy var bibleButton = Create.baseButton("Biblía Sagrada", titleColor: .black,
+                                                     backgroundColor: Assets.Colors.green) {_ in
 //            self.navigationController?.pushViewController(self.booksviewcontroller, animated: true)
         self.navigationController?.navigate(to: self.booksviewcontroller)
-    })
-    
+    }
     private lazy var verseLabel = Create.label("Versículo do dia.", font: nil)
-    
     private lazy var verseDayLabel: UILabel = {
        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -34,47 +30,30 @@ class IntroViewController: UIViewController, SetupView {
         label.text = "O cavalo prepara-se para o dia da batalha, mas a vitória vem do SENHOR.\n\nProvérbios 21:31 ARA"
         return label
     }()
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         setupConstraints()
-        
     }
-    
     func setupView() {
         view.backgroundColor = .darkGray
         verseDayLabel.textColor = .white
         view.addSubviews([straighteningButton, bibleButton, verseLabel, verseDayLabel])
     }
-    
     func setupConstraints() {
-        
         NSLayoutConstraint.activate([
-        
             straighteningButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
             straighteningButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             straighteningButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            
             verseLabel.topAnchor.constraint(equalTo: straighteningButton.bottomAnchor, constant: 50),
             verseLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            
             verseDayLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             verseDayLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             verseDayLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             verseDayLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            
             bibleButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
             bibleButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            bibleButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            
-        
-            
-        
+            bibleButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
         ])
-        
     }
-    
 }
