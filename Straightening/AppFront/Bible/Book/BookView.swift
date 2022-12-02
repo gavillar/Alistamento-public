389fabeae2617d5a4b91f64c09c9bd354cadc5fe
Josicleison <engenhariaeletricaquinto@gmail.com>
2022-12-02 13:05:01 -0300

    view separada da view controller

--- Straightening/AppFront/Bible/Book/BookView.swift
+++ Straightening/AppFront/Bible/Book/BookView.swift
@@ -8,10 +8,47 @@
 import UIKit
 
 class BookView: UIView {
+// MARK: - variables
+    lazy var versesLabel: UILabel = {
+        let versesLabel = UILabel()
+        versesLabel.text = ""
+        versesLabel.numberOfLines = 0
+        return versesLabel
+    }()
+    lazy var bookCollectionView: BookCollectionView = {
+        let bookCollectionView = BookCollectionView()
+        return bookCollectionView
+    }()
+    private lazy var versesScroll: UIScrollView = {
+        let versesScroll = UIScrollView()
+        versesScroll.translatesAutoresizingMaskIntoConstraints = false
+        versesScroll.turnIntoAList(of: [versesLabel])
+        return versesScroll
+    }()
+// MARK: - overrides
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupView()
+        setupConstraints()
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+// MARK: - setup
+    func setupView() {
+        backgroundColor = Assets.Colors.whiteBlack
+        addSubviews([bookCollectionView, versesScroll])
+    }
+    func setupConstraints() {
+        addConstraints([
+            bookCollectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            bookCollectionView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            bookCollectionView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            bookCollectionView.heightAnchor.constraint(equalToConstant: frame.width*0.1),
+            versesScroll.topAnchor.constraint(equalTo: bookCollectionView.bottomAnchor),
+            versesScroll.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            versesScroll.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            versesScroll.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
--- Straightening/AppFront/Bible/Book/BookViewController.swift
+++ Straightening/AppFront/Bible/Book/BookViewController.swift
@@ -10,74 +10,41 @@ import UIKit
 final class BookViewController: UIViewController {
 // MARK: - variables
     private let bookViewModel: BookViewModel
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    private lazy var versesLabel: UILabel = {
-        let versesLabel = UILabel()
-        versesLabel.text = ""
-        versesLabel.numberOfLines = 0
-        return versesLabel
-    }()
-    private lazy var versesScroll: UIScrollView = {
-        let versesScroll = UIScrollView()
-        versesScroll.translatesAutoresizingMaskIntoConstraints = false
-        versesScroll.turnIntoAList(of: [versesLabel])
-        return versesScroll
-    }()
-    private lazy var bookCollectionView: BookCollectionView = {
-        let bookCollectionView = BookCollectionView()
-        bookCollectionView.bookCollectionViewDelegate = self
-        return bookCollectionView
-    }()
+    private lazy var bookView = BookView(frame: view.frame)
 // MARK: - init
     init(_ booksElements: BooksElements) {
         self.bookViewModel = BookViewModel(booksElements)
         super.init(nibName: nil, bundle: nil)
-        bookViewModel.delegate = self
-        bookViewModel.updateBook()
-        bookViewModel.updateLabel()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 // MARK: - overrides
     override func loadView() {
         super.loadView()
-        setupView()
-        setupConstraints()
-    }
-// MARK: - setup
-    func setupView() {
+        bookViewModel.delegate = self
+        bookViewModel.updateBook()
+        bookViewModel.updateLabel()
+        bookView.bookCollectionView.bookCollectionViewDelegate = self
+        view = bookView
         title = bookViewModel.title
-        view.backgroundColor = Assets.Colors.whiteBlack
-        view.addSubviews([bookCollectionView, versesScroll])
-    }
-    func setupConstraints() {
-        view.addConstraints([
-            bookCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            bookCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            bookCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            bookCollectionView.heightAnchor.constraint(equalToConstant: view.frame.width*0.1),
-            versesScroll.topAnchor.constraint(equalTo: bookCollectionView.bottomAnchor),
-            versesScroll.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            versesScroll.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            versesScroll.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
     }
 }
 
 extension BookViewController: BookViewModelDelegate {
     func unfreezeCollection() {
-        bookCollectionView.isUserInteractionEnabled = true
+        bookView.bookCollectionView.isUserInteractionEnabled = true
     }
     func updateLabel(text: String) {
         Task {[weak self] in
-            self?.versesLabel.text?.append(text)
+            self?.bookView.versesLabel.text?.append(text)
         }
     }
 }
 
 extension BookViewController: BookCollectionViewDelegate {
     func collectionView(didSelectItemAt indexPath: IndexPath) {
-        versesLabel.text = ""
+        bookView.versesLabel.text = ""
         bookViewModel.updateLabel(indexPath.row)
     }
     func numberOfItemsInSection() -> Int {
