3e645519a70a5375a2fe63fed2765a018ed0e1f8
Josicleison <engenhariaeletricaquinto@gmail.com>
2022-12-07 10:15:38 -0300

    bugFix tests

---
+++ Straightening/AppUtils/ExtensionsTests.swift
@@ -0,0 +1,16 @@
+//
+//  StraighteningTests.swift
+//  StraighteningTests
+//
+//  Created by Josicleison Elves on 16/09/1401 AP.
+//
+
+import XCTest
+
+@testable import Straightening
+
+final class ExtensionsTests: XCTestCase {
+    func formatMask() {
+        print(String.formatMask("(##)#####-####"))
+    }
+}
