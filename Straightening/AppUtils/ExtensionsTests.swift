6ae22e4fbabed8d096fdc2ea4661f3ac7c755f91
Josicleison <engenhariaeletricaquinto@gmail.com>
2022-12-07 11:38:44 -0300

    modifyed name

--- Straightening/AppUtils/ExtensionsTests.swift
+++ Straightening/AppUtils/ExtensionsTests.swift
@@ -11,19 +11,19 @@ import XCTest
 
 final class ExtensionsTests: XCTestCase {
     func testFormatMask() {
-        let test = {(numbers: [String: String]) in
+        let phoneTest = {(numbers: [String: String]) in
             for number in numbers {
                 XCTAssertTrue(number.key.formatMask(number.value).first == "(" &&
                               number.key.formatMask(number.value).contains(")") &&
                               number.key.formatMask(number.value).contains("-"))
             }
         }
-        test(["123456791011": "(##)#####-####",
-              "123456791": "(##)#####-####",
-              "12345679101a1": "(##)#####-####",
-              "(12)34567-91011": "(##)#####-####",
-              "(1)23-456791011": "(##)#####-####",
-              "1###23456791011": "(##)#####-####",
-              "123456791011123": "(##)#####-####"])
+        phoneTest(["123456791011": "(##)#####-####",
+                   "123456791": "(##)#####-####",
+                   "12345679101a1": "(##)#####-####",
+                   "(12)34567-91011": "(##)#####-####",
+                   "(1)23-456791011": "(##)#####-####",
+                   "1###23456791011": "(##)#####-####",
+                   "123456791011123": "(##)#####-####"])
     }
 }
