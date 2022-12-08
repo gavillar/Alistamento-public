e46098ddfa1b8776c2242d3c4da4b71ea5d9ceb9
Josicleison <engenhariaeletricaquinto@gmail.com>
2022-12-08 10:27:37 -0300

    attenpting swiftLint warnings

--- Straightening/AppFront/Home/HomeViewController.swift
+++ Straightening/AppFront/Home/HomeViewController.swift
@@ -8,14 +8,12 @@ import Foundation
 import UIKit
 
 class HomeViewController: UIViewController {
-    private lazy var ScrollView: UIScrollView = {
+    private lazy var scrollView: UIScrollView = {
         let scroll = UIScrollView()
         let stackview = scroll.turnIntoAList(of: [homeView])
-        
         scroll.translatesAutoresizingMaskIntoConstraints = false
         return scroll
     }()
-    
     private lazy var welcomeLabel: UILabel = {
         let label = Create.labelTitle("Bem Vindo Jacaré")
         return label
@@ -27,26 +25,23 @@ class HomeViewController: UIViewController {
     }()
     override func loadView() {
         super.loadView()
-        view.addSubview(ScrollView)
+        view.addSubview(scrollView)
         homeView.addSubviews([welcomeLabel])
         setupConstraints()
         view.layer.addGradientBackground()
         navigationController?.navigationBar.isUserInteractionEnabled = false
         navigationController?.navigationBar.isHidden = true
     }
-    private func setupConstraints(){
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
-            ScrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            ScrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            ScrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            ScrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             homeView.heightAnchor.constraint(equalTo: view.heightAnchor),
-            
-            welcomeLabel.topAnchor.constraint(equalTo: homeView.topAnchor,constant: 20),
-            welcomeLabel.leadingAnchor.constraint(equalTo: homeView.leadingAnchor,constant: 12),
-            welcomeLabel.trailingAnchor.constraint(equalTo: homeView.trailingAnchor,constant: 12),
-            
+            welcomeLabel.topAnchor.constraint(equalTo: homeView.topAnchor, constant: 20),
+            welcomeLabel.leadingAnchor.constraint(equalTo: homeView.leadingAnchor, constant: 12),
+            welcomeLabel.trailingAnchor.constraint(equalTo: homeView.trailingAnchor, constant: 12),
         ])
     }
 }
