b23aaf3fd80b1f085ab58901a5cd5aac0220d3db
VillarDeveloper <villar.developer@gmail.com>
2023-01-09 06:19:58 -0800

    get method.

--- Straightening/AppFront/Intro/Login/Register/Address/RegisterAdressViewModel.swift
+++ Straightening/AppFront/Intro/Login/Register/Address/RegisterAdressViewModel.swift
@@ -24,8 +24,8 @@ class RegisterAdressViewModel {
     func getApiCep() {
         Task { [weak self] in
             guard self != nil else {return}
-            guard let data = await Network.call(from: Network.EndPoints.cepInformation(cep)) else {return}
-            guard let cep = await Network.decode(RegisterModel.Cep.self, from: data) else {return}
+            guard let data = await Network.get(from: Network.EndPoints.cepInformation(cep)) else {return}
+            guard let cep = Network.decode(RegisterModel.Cep.self, from: data) else {return}
             print(cep)
             formViewModelDelegate?.sendCep(cep: cep)
             updateFormDelegate?.updateForm()
